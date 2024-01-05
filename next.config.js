94a24fd0974af3f12281edac3aa08ec989983fae
Junaire Edris Buico <juned.buico@gmail.com>
2024-01-05 22:20:28 +0800

    added output in next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.pexels.com", port: "" },
